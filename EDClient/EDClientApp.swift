d428c3fdb020434dc43940f2d58d7d1343d1ccd0
“Oleksii <oleksiychumak2004@gmail.com>
2024-06-13 11:47:11 +0300

    Initial Commit

---
+++ EDClient/EDClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDClientApp.swift
+//  EDClient
+//
+//  Created by Oleksii Chumak on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EDClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
